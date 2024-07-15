39409157b0df6b4319c152f73af535106a6b130f
Nam1934 <nam.2174802010895@vanlanguni.vn>
2024-07-15 12:36:26 +0700

    verifyemail_update

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -16,13 +16,17 @@ module.exports = function (validator: any) {
 
       next();
     } catch (err: any) {
-      console.log("testtttttt", err);
-      //* Pass err to next
-      //! If validation error occurs call next with HTTP 422. Otherwise HTTP 500
-      if (err.isJoi) {
-        return res.status(422).json({ message: err.message });
-      }
-      return res.status(500).json({ message: err.message });
+      console.log("Validation Error: ", err);
+
+      const errStatus = err.isJoi ? 422 : 500;
+      const errMsg = err.message || "Something went wrong";
+
+      res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg,
+        stack: process.env.NODE_ENV === "development" ? err.stack : {},
+      });
     }
   };
 };
