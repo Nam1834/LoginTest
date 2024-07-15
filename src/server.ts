80f3249afce4b3a830276ba662347716e85e46bb
Nam1934 <nam.2174802010895@vanlanguni.vn>
2024-07-15 13:13:06 +0700

    cors fix

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,18 @@ import express from "express";
 import { emailNotifyCronJob } from "./services/todo.service";
 import { now } from "lodash";
 import ErrorHandler from "./middleware/error_handle.middleware";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 
 sequelizeInit();
+
+app.use(cors());
+
 apiroute(app);
+
 emailNotifyCronJob();
 
 app.use(ErrorHandler);
